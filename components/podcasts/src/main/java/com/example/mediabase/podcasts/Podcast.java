72032ce7de1abead5242f207703e30898f87c003
Gordon Force <gordon.force@perficient.com>
2019-12-04 17:43:51 -0800

    Upgraded code to work with Spring Boot 2.1.10, PCF 2.7.4, and MySQL for PCF v2

--- components/podcasts/src/main/java/com/example/mediabase/podcasts/Podcast.java
+++ components/podcasts/src/main/java/com/example/mediabase/podcasts/Podcast.java
@@ -16,7 +16,7 @@ public class Podcast implements Serializable {
     private String url;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Podcast() {
